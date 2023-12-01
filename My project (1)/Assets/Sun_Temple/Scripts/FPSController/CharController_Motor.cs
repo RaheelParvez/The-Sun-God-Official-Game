b5bc597cb3cf0b68bc41196472065255a5436864
nutball <21913@students.isb.ac.th>
2023-12-01 10:42:22 +0700

    MOVEMENT

--- My project (1)/Assets/Sun_Temple/Scripts/FPSController/CharController_Motor.cs
+++ My project (1)/Assets/Sun_Temple/Scripts/FPSController/CharController_Motor.cs
@@ -2,86 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SunTemple{
+namespace SunTemple
+{
 
 
-	public class CharController_Motor : MonoBehaviour {
+	public class CharController_Motor : MonoBehaviour
+	{
 
-		public float speed = 10.0f;
-		public float sensitivity = 60.0f;
-		CharacterController character;
-		public GameObject cam;
-		float moveFB, moveLR;	
-		float rotHorizontal, rotVertical;
-		public bool webGLRightClickRotation = true;
-		float gravity = -9.8f;
-
-		//string debugText;
+		private CharacterController controller;
+		private Vector3 playerVelocity;
+		private bool groundedPlayer;
+		private float playerSpeed = 2.0f;
+		private float jumpHeight = 1.0f;
+		private float gravityValue = -9.81f;
 
 
-		void Start(){
+		void Start()
+		{
 
-			character = GetComponent<CharacterController> ();
-
-			webGLRightClickRotation = false;
-
-			if (Application.platform == RuntimePlatform.WebGLPlayer) {
-				webGLRightClickRotation = true;
-				sensitivity = sensitivity * 1.5f;
-			}
+			controller = GetComponent<CharacterController>();
 
 
 		}
 
-
-
-
-
-		void FixedUpdate(){
-			moveFB = Input.GetAxis ("Horizontal") * speed;
-			moveLR = Input.GetAxis ("Vertical") * speed;
-
-			rotHorizontal = Input.GetAxisRaw ("Mouse X") * sensitivity;
-			rotVertical = Input.GetAxisRaw ("Mouse Y") * sensitivity;
-
-
-			Vector3 movement = new Vector3 (moveFB, gravity, moveLR);
-
-
-			if (webGLRightClickRotation) {
-				if (Input.GetKey (KeyCode.Mouse0)) {
-					CameraRotation (cam, rotHorizontal, rotVertical);
-				}
-			} else if (!webGLRightClickRotation) {
-				CameraRotation (cam, rotHorizontal, rotVertical);
+		void Update()
+		{
+			groundedPlayer = controller.isGrounded;
+			if (groundedPlayer && playerVelocity.y < 0)
+			{
+				playerVelocity.y = 0f;
 			}
 
-			movement = transform.rotation * movement;
-			character.Move (movement * Time.fixedDeltaTime);
-		}
+			Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			controller.Move(move * Time.deltaTime * playerSpeed);
 
-
-
-
-
-	
-
-		void CameraRotation(GameObject cam, float rotHorizontal, float rotVertical){	
-
-			transform.Rotate (0, rotHorizontal * Time.fixedDeltaTime, 0);
-			cam.transform.Rotate (-rotVertical * Time.fixedDeltaTime, 0, 0);
-
-
-
-			if (Mathf.Abs (cam.transform.localRotation.x) > 0.7) {
-
-				float clamped = 0.7f * Mathf.Sign (cam.transform.localRotation.x); 
-
-				Quaternion adjustedRotation = new Quaternion (clamped, cam.transform.localRotation.y, cam.transform.localRotation.z, cam.transform.localRotation.w);
-				cam.transform.localRotation = adjustedRotation;
+			if (move != Vector3.zero)
+			{
+				gameObject.transform.forward = move;
 			}
 
 
+			if (Input.GetButtonDown("Jump") && groundedPlayer)
+			{
+				playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+			}
+			playerVelocity.y += gravityValue * Time.deltaTime;
+			controller.Move(playerVelocity * Time.deltaTime);
 		}
 
 
