11e92377cb0082301d2150772cfaba3a9038cfa7
nutball <21913@students.isb.ac.th>
2023-12-06 09:14:15 +0700

    boss
    boss bar and a few more things
    you have to hold to shoot now
    no animation for that yet

--- My project (1)/Assets/aroor.cs
+++ My project (1)/Assets/aroor.cs
@@ -8,6 +8,8 @@ public class aroor : MonoBehaviour
     public int weee;
     public float off;
     public float offy;
+    float timer;
+    float holdDur = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +21,33 @@ public class aroor : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject naw = Instantiate(arr) as GameObject;
-            Vector3 high = transform.position;
-            high += Camera.main.transform.forward * off;
-            high.y += offy;
-            naw.transform.position = high;
-            Rigidbody rb = naw.GetComponent<Rigidbody>();
-            rb.velocity = Camera.main.transform.forward * weee;
+            timer = Time.time;
         }
+        else if (Input.GetMouseButton(0))
+        {
+            if (Time.time - timer > holdDur)
+            {
+               
+                timer = float.PositiveInfinity;
+                Debug.Log("ready");
+                Fire();
+
+              
+            }
+        }
+        else
+        {
+            timer = float.PositiveInfinity;
+        }
+    }
+    void Fire()
+    {
+        GameObject naw = Instantiate(arr) as GameObject;
+        Vector3 high = transform.position;
+        high += Camera.main.transform.forward * off;
+        high.y += offy;
+        naw.transform.position = high;
+        Rigidbody rb = naw.GetComponent<Rigidbody>();
+        rb.velocity = Camera.main.transform.forward * weee;
     }
 }
---
+++ My project (1)/Assets/bosshealth.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Bossbar : MonoBehaviour
+{
+    public Image bar;
+    public float bhp = 100f;
+    public float damage;
+    public GameObject boss;
+    public bool oww;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit");
+        if (other.gameObject.CompareTag("arrow"))
+        {
+            dmg(damage);
+            Debug.Log("dmg" + damage);
+            if (oww)
+            {
+                boss.GetComponent<Animator>().Play("Mk_hitfFfront");
+            }
+            
+        }
+    }
+
+    public void dmg(float dam)
+    {
+        bhp -= dam;
+        if (bhp < 0) { bhp = 0; }
+        bar.fillAmount = bhp / 100f;
+    }
+    public void heal(float heal)
+    {
+        bhp += heal;
+        if (bhp > 100) { bhp = 100; }
+        bar.fillAmount = bhp / 100f;
+    }
+}
